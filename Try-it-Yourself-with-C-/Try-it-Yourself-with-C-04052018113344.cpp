40632dda15f168fe8089a50540540eab60d96663
Raji <rajicutee@gmail.com>
2018-05-04 17:03:45 +0530

    File creation

---
+++ Try-it-Yourself-with-C-/Try-it-Yourself-with-C-04052018113344.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main()
+{
+    string str;
+    cin>>str;
+    bool flg = true;
+    int i = 0;
+    while(i < str.length()/2)
+    {
+            if(str[i] != str[str.length()-i-1])
+               flg = false;
+        i++;
+    }
+    if(flg)
+    cout << "YES" << endl;
+    else
+    cout<< "NO" << endl;
+    return 0;
+}
